6b29386120efd5b3b9fbc5886a92a5f62776b2b2
Abdullah <133237044+IMA2024@users.noreply.github.com>
2023-07-12 01:11:28 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use("/business", businessRouter);
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("Connected to MongoDB")
 }).then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 8080, () => {
         console.log("Server is running.")
     })
 })
